63b845a7195ffc0eb8cc65fa429435f1bc91313b
Ali Rahjouei, Dr. rer. nat <88965480+arahjou@users.noreply.github.com>
2024-02-15 21:08:52 +0100

    Update Version.py

--- Version.py
+++ Version.py
@@ -1,3 +1,5 @@
+import random
+
 board =["-", "-", "-",
         "-", "-", "-",
         "-", "-", "-"]
@@ -25,11 +27,72 @@ def playerInput(board):
 
 
 # check for win or lost
-        
 def checkHorizontle(board):
-    
+        global winner
+        if board[0] == board[1] == board[2] and board[0] != "-":
+                winner = board[0]
+                return True
+        elif board[3] == board[4] == board[5] and board[3] != "-":
+                winner = board[3]
+                return True
+        elif board[6] == board[7] == board[8] and board[6] != "-":
+                winner = board[6]
+                return True
+                
+def checkRow(board):
+        global winner
+        if board[0] == board[3] == board[6] and board[0] != "-":
+                winner = board[0]
+                return True
+        elif board[1] == board[4] == board[7] and board[1] != "-":
+                winner = board[1]
+                return True
+        elif board[2] == board[5] == board[8] and board[2] != "-":
+                winner = board[2]
+                return True
 
+def checkDiag(board):
+        global winner
+        if board[0] == board[4] == board[8] and board[0] != "-":
+                winner = board[0]
+                return True
+        elif board[2] == board[4] == board[6] and board[1] != "-":
+                winner = board[2]
+                return True
+
+def checkTie(bord):
+        global gameRunning
+        if "-" not in board:
+                printBoard(board)
+                print("It is a tie!")
+                gameRunning = False
+
+def checkWin():
+        if checkDiag(board) or checkRow(board) or checkHorizontle(board)
+                print(f"THe winner is {winner}")
+
+
+def switchPlayer():
+        global currentPlayer
+        if currentPlayer == "X":
+                currentPlayer = "O"
+        else:
+                currentPlayer = "X"
+# computer
+def computer(board):
+        while currentPlayer == "O":
+                position = random.randint(0, 8)
+                if board[position] == "-":
+                        board[position] == "O"
+                        switchPlayer()
+        
 # check for win or tie again1
 while gameRunning:
-    printBoard(board)
-    playerInput(board)
+        printBoard(board)
+        playerInput(board)
+        checkWin()
+        checkTie()
+        switchPlayer()
+        computer(board)
+        checkWin()
+        checkTie()
